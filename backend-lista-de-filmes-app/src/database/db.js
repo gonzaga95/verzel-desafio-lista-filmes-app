5fde333001563e26b492093f064e06450811d2e7
Carlos E. Gonzaga <gonzagas.dudu@gmail.com>
2025-10-23 21:06:24 -0400

    refactor: store MongoDB URI in a constant for improved readability

--- backend-lista-de-filmes-app/src/database/db.js
+++ backend-lista-de-filmes-app/src/database/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+const DB_URI = process.env.MONGODB_URI;
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(DB_URI);
         console.log('Conectado ao Banco de Dados com sucesso!');
     } catch (error) {
         console.error('Erro ao conectar ao Banco de Dados:', error.message);
